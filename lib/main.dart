a0e52206e7bc059807d01331712c8654be73f23f
phavour <phavyolo@gmail.com>
2022-07-23 12:55:49 +0100

    #feat: added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_calcutor_with_getx/utils/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
+      theme: theme(),
       home: Container(),
     );
   }
