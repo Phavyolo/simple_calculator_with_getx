456712b8450b553dcf2bdb589cb7e06839a5f930
phavour <phavyolo@gmail.com>
2022-07-23 12:57:20 +0100

    #feat: added routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 final Map<String, WidgetBuilder> routes = {};
-=======
-final Map<String, WidgetBuilder> routes = {
-};
->>>>>>> origin/master
+
+
